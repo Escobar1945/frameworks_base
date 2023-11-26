b228bab475e1f9609635e00150dc48d96564e5e1
Pranav Vashi <neobuddy89@gmail.com>
2023-11-26 12:28:14 +0530

    Add optional weather condition text on lockscreen
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7051,6 +7051,11 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_TEXT = "lockscreen_weather_text";
+
+        /**
          * Whether to show floating rotation button
          * @hide
          */
